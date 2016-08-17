2bf3d1ab82bf5e1674f0796777c310850fefedf9
Laurens De Cock <laurens.decock@student.odisee.be>
2016-08-17 14:17:59 +0200

    Made admin panel somewhat responsive

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -2,29 +2,30 @@
 <html>
 	<head>
 		<title>Administrator Panel &ndash; As-Tegel</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="{{ asset('css/app.css') }}"/>
 		@yield('css')
 	</head>
 	<body>
-		<div class="toolbar">
+		<div class="toolbar toolbar_admin">
 			<div class="toolbar_content">
 				<div class="toolbar_left">
 					<div class="toolbar_languages"><a href="{{ route('home') }}">Terug naar site</a> - <a href="{{ route('admin') }}">Admin Panel</a></div>
 				</div>
 				<div class="toolbar_right">
-					<div class="toolbar_contact">
+					<div class="toolbar_admin_item">
 						<a @if($admin_menu=='gebruikers') class="admin_active" @endif href="{{ route('admin_useroverview') }}">Gebruikers</a>
 					</div>
-					<div class="toolbar_contact">
+					<div class="toolbar_admin_item">
 						<a @if($admin_menu=='producten') class="admin_active" @endif href="{{ route('admin_productoverview') }}">Producten</a>
 					</div>
-					<div class="toolbar_contact">
+					<div class="toolbar_admin_item">
 						<a @if($admin_menu=='categorie') class="admin_active" @endif href="{{ route('admin_categorieoverview') }}">Categorie</a>
 					</div>
-					<div class="toolbar_contact">
+					<div class="toolbar_admin_item">
 						<a @if($admin_menu=='subcategorie') class="admin_active" @endif href="{{ route('admin_subcategorieoverview') }}">Subcategorie</a>
 					</div>
-					<div class="toolbar_contact">
+					<div class="toolbar_admin_item">
 						<a @if($admin_menu=='fotoupload') class="admin_active" @endif href="{{ route('admin') }}/fotoupload">Foto Uploaden</a>
 					</div>
 					@if(Auth::check())
