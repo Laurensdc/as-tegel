8f0840da408d9734a05efe5e84cf84f65629cf8f
Laurens De Cock <laurens.decock@student.odisee.be>
2016-08-17 02:18:15 +0200

    Made elements on homepage fully clickable

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,38 +2,45 @@
 
 
 @section('homecontent')
-    <article class="home_carius">
-        <div class="home_caption">
-            <a href="{{ route('producten') }}/specialiteiten">Unieke Specialiteiten</a>
-        </div>
-    </article>
+    <a href="{{ route('producten') }}/specialiteiten">
+        <article class="home_carius">
+            <div class="home_caption">
+                <span>Unieke Specialiteiten</span>
+            </div>
+        </article>
+    </a>
 
-    <article class="home_keramiek">
-        <div class="home_caption">
-            <a href="{{ route('producten') }}/keramiek">Keramische Natuursteen</a>
-        </div>
-    </article>
+    <a href="{{ route('producten') }}/keramiek">
+        <article class="home_keramiek">
+            <div class="home_caption">
+                <span>Keramische Natuursteen</span>
+            </div>
+        </article>
+    </a>
 
+    <a href="{{ route('producten') }}/vietnamees">
+        <article class="home_vietnamees">
+            <div class="home_caption">
+                <span>Vietnamese natuursteen</span>
+            </div>
+        </article>
+    </a>
 
-    <article class="home_vietnamees">
-        <div class="home_caption">
-            <a href="{{ route('producten') }}/vietnamees">Vietnamese natuursteen</a>
-        </div>
+    <a href="{{ route('producten') }}/chinees">
+        <article class="home_chinees">
+            <div class="home_caption">
+                <span>Chinese natuursteen</span>
+            </div>
+        </article>
+    </a>
 
-    </article>
-
-    <article class="home_chinees">
-        <div class="home_caption">
-            <a href="{{ route('producten') }}/chinees">Chinese natuursteen</a>
-        </div>
-    </article>
-
-    <article class="home_indisch">
-        <div class="home_caption">
-            <a href="{{ route('producten') }}/indisch">Indische natuursteen</a>
-        </div>
-    </article>
-
+    <a href="{{ route('producten') }}/indisch">
+        <article class="home_indisch">
+            <div class="home_caption">
+                <span>Indische natuursteen</span>
+            </div>
+        </article>
+    </a>
 
 
 @endsection
