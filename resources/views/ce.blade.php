11e348b2474a58a0e123d6a6140f83118881edbe
Laurens De Cock <laurens.decock@student.odisee.be>
2016-08-18 18:23:02 +0200

    Style CE page

--- resources/views/ce.blade.php
+++ resources/views/ce.blade.php
@@ -3,10 +3,12 @@
 @section('content')
 	
 	<div class='contact_content'>
-		<h2>Contactinformatie</h2>
-		<p>Om het vrije verkeer van bouwproducten te verbeteren, heeft de Europese Commissie de wetgeving die van toepassing is op deze sector, met name de Europese Richtlijn 89/106/EEG, herzien. Deze herziening, voor het verhandelen van bouwproducten, heeft een concrete vorm gekregen in de Europese verordening EU nr. 305/2011. De Verordening bepaalt dat vanaf 1 juli 2013, wanneer bouwproducten door de fabrikant verhandeld worden, ze verplicht worden het document "Prestatieverklaring" (DoP = Declaration of Performance), opgesteld op basis van de geharmoniseerde technische specificaties, beschikbaar te stellen. Dit laatste (DoP) vervangt de vroegere "Declaration of Conformity".
-
-U kan de gewenste DoP opvragen in pdf-formaat door het invoeren van het nummer op uw leveringsbon.</p>
+		<h2>CE Markering</h2>
+		<p>Om het vrije verkeer van bouwproducten te verbeteren, heeft de Europese Commissie de wetgeving die van toepassing is op deze sector, met name de Europese Richtlijn 89/106/EEG, herzien.</p>
+        <p> Deze herziening, voor het verhandelen van bouwproducten, heeft een concrete vorm gekregen in de Europese verordening EU nr. 305/2011.</p>
+        <p>De Verordening bepaalt dat vanaf 1 juli 2013, wanneer bouwproducten door de fabrikant verhandeld worden, ze verplicht worden het document "Prestatieverklaring" (DoP = Declaration of Performance), opgesteld op basis van de geharmoniseerde technische specificaties, beschikbaar te stellen.</p>
+        <p>Dit laatste (DoP) vervangt de vroegere "Declaration of Conformity".</p>
+        <p>U kan de gewenste DoP opvragen wanneer u een bestelling plaatst.</p>
 
 	</div>
 
