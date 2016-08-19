9d9b3d76ba55792d6bfbbef5484886c4d302dff0
Laurens De Cock <laurens.decock@student.odisee.be>
2016-08-19 19:33:33 +0200

    Add space on product page

--- local/resources/views/products.blade.php
+++ local/resources/views/products.blade.php
@@ -58,7 +58,7 @@
                 <form action="" method="post" name="form">
                     {{ csrf_field() }}
                     <input type="hidden" value="{{ $p->id }}" name="prod_id">
-                    <p>{{ trans('cont.add') }} <input type="number" name="vierkantemeter" min="1" max="999" value="0" required/>x 1{!! $p->eenheid !!} {{ trans('cont.toorder') }}.</p> 
+                    <p>{{ trans('cont.add') }} <input type="number" name="vierkantemeter" min="1" max="999" value="0" required/>x 1 {!! $p->eenheid !!} {{ trans('cont.toorder') }}.</p> 
                     <p><input type="submit" clas="btn btn_cta" value="{{ trans('cont.order') }}"/></p>
                 </form>
 			</article>
