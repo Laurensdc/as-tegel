4c47c0c730be1d7179ac322673f27c994aaedc38
Laurens De Cock <laurens.decock@student.odisee.be>
2016-09-05 11:49:01 +0200

    Update contact page with map

--- local/resources/views/contact.blade.php
+++ local/resources/views/contact.blade.php
@@ -26,10 +26,18 @@
 			<li><span class='contact_content_left'>Telefoonnummer: </span>0475/27.37.17</li>
 		</ul>
 
-		<h3>Vragen over de website? Een fout gevonden?</h3>
+        <div class='contact_gap'></div>
+
+		<h2>Vragen over de website? Een fout gevonden?</h2>
 		<ul>
 			<li><span class='contact_content_left'>Contacteer webmaster: </span><a href="mailto:webmaster@as-tegel.be">webmaster@as-tegel.be</a></li>
 		</ul>
+
+        <div class='contact_gap'></div>
+
+        <h2>Vind ons op de kaart</h2>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2516.495805417143!2d3.372398215949928!3d50.89604146303791!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c33e8a0ecabc8f%3A0x464d2387fce1b9ff!2sPitantiestraat+90%2C+8792+Waregem!5e0!3m2!1sen!2sbe!4v1473067844761" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+
 
 	</div>
 
